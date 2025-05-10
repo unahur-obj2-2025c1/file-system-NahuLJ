a6ca5e2c2c404925776fbae99af937b3147f5748
Nahuel Lautaro Jimenez <nahuellautaro.jimenez@estudiantes.unahur.edu.ar>
2025-05-10 16:00:25 -0300

    Hice archivo mas pesado y tamaño total

--- src/main/java/ar/edu/unahur/obj2/Carpeta.java
+++ src/main/java/ar/edu/unahur/obj2/Carpeta.java
@@ -3,20 +3,34 @@ package ar.edu.unahur.obj2;
 import java.util.Comparator;
 import java.util.List;
 
-public class FileSystem {
+public class Carpeta implements IElemento{
+	private String nombre;
 	private List<IElemento> archivos;
 	
-	public FileSystem(List<IElemento> archivos) {
+	public Carpeta(String nombre, List<IElemento> archivos) {
+		this.nombre = nombre;
 		this.archivos = archivos;
 	}
 	
-	public Integer tamanioTotal() {
+	@Override
+	public String nombre() {
+		return nombre;
+	}
+
+	@Override
+	public Integer tamanio() {
 		return archivos.stream().mapToInt(archivo -> archivo.tamanio()).sum();
 	}
-	
+
+	@Override
+	public void mostrar(Integer identacion) {
+		
+	}
+
+	@Override
 	public IElemento archivoMasPesado() {
 		return archivos.stream().map(archivo -> archivo.archivoMasPesado())
 				.max(Comparator.comparingInt(archivo -> archivo.tamanio())).orElseThrow();
 	}
-	
+
 }
